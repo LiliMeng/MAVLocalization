40d87f48639739b5522546f65e09c45186e692e7
LiliMeng <lilimeng1103@gmail.com>
2016-02-16 12:17:59 -0800

    Create RTAB_incremental_vocabulary.cpp

---
+++ loop_closure/RTAB_incremental_vocabulary.cpp
@@ -0,0 +1,12 @@
+//
+//  RTAB_incremental_vocabulary.cpp
+//  MAVGoogleImageMatching
+//
+//  Created by jimmy & Lili on 2016-02-13.
+//  Copyright © 2016 jimmy & Lili. All rights reserved.
+//
+
+#include "RTAB_incremental_vocabulary.hpp"
+
+
+
