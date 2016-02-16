601ab9279923ee10ddb049cdac21a99b2cccfe98
LiliMeng <lilimeng1103@gmail.com>
2016-02-16 12:18:34 -0800

    Create UT_RTAB.hpp

---
+++ loop_closure/UT_RTAB.hpp
@@ -0,0 +1,17 @@
+//
+//  UT_RTAB.hpp
+//  MAVGoogleImageMatching
+//
+//  Created by jimmy&Lili on 2016-02-13.
+//  Copyright © 2016 jimmy&Lili. All rights reserved.
+//
+
+#ifndef UT_RTAB_cpp
+#define UT_RTAB_cpp
+
+// RTAB unit test
+void test_RTAB();
+void test_feature_extraction();
+void test_incremental_vocabulary();
+
+#endif /* UT_RTAB_cpp */
